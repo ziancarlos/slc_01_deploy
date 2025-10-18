86e32d1c6ef55cb3d2ac59fb664ff7dfdac391e2
Zian Carlos Wong <ziancrlswong@gmail.com>
2025-10-18 12:50:36 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,22 @@
+module p2_slc_1
+
+go 1.25.3
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.10 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.28.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.6 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.42.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/julienschmidt/httprouter"
+)
+
+type Product struct {
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       int    `json:"price"`
+}
+
+type WebResponse struct {
+	Data any `json:"data"`
+}
+
+func main() {
+	db, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+	defer db.Close(context.Background())
+
+	router := httprouter.New()
+
+	router.GET("/products", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		rows, err := db.Query(context.Background(), "SELECT id, name, description, price FROM products")
+
+		if err != nil {
+			panic(err)
+		}
+		defer rows.Close()
+
+		products := []Product{}
+		for rows.Next() {
+			product := Product{}
+			rows.Scan(&product.Id, &product.Name, &product.Description, &product.Price)
+			products = append(products, product)
+		}
+
+		writer.Header().Add("Content-Type", "application/json")
+		writer.WriteHeader(200)
+		encoder := json.NewEncoder(writer)
+		encoder.Encode(WebResponse{
+			Data: products,
+		})
+	})
+
+	router.GET("/products/:id", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		id := params.ByName("id")
+		rows, err := db.Query(context.Background(), "SELECT id, name, description, price FROM products WHERE id = $1", id)
+
+		if err != nil {
+			panic(err)
+		}
+		defer rows.Close()
+
+		product := Product{}
+		if rows.Next() {
+			rows.Scan(&product.Id, &product.Name, &product.Description, &product.Price)
+		}
+
+		writer.Header().Add("Content-Type", "application/json")
+		writer.WriteHeader(200)
+		encoder := json.NewEncoder(writer)
+		encoder.Encode(WebResponse{
+			Data: product,
+		})
+	})
+
+	router.POST("/products", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		product := Product{}
+		decoder := json.NewDecoder(request.Body)
+		err := decoder.Decode(&product)
+		fmt.Println(err)
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = db.Exec(context.Background(), "INSERT INTO  products (name, description, price) VALUES($1, $2, $3)", product.Name, product.Description, product.Price)
+		if err != nil {
+			panic(err)
+		}
+
+		writer.Header().Add("Content-Type", "application/json")
+		writer.WriteHeader(200)
+		encoder := json.NewEncoder(writer)
+		encoder.Encode(WebResponse{
+			Data: "Successfuly created new products",
+		})
+	})
+
+	router.PUT("/products/:id", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		id := params.ByName("id")
+		product := Product{}
+		decoder := json.NewDecoder(request.Body)
+		decoder.Decode(&product)
+
+		_, err := db.Exec(context.Background(), "UPDATE products SET name=$1, price=$2, description=$3 WHERE id=$4", product.Name, product.Price, product.Description, id)
+		if err != nil {
+			panic(err)
+		}
+
+		writer.Header().Add("Content-Type", "application/json")
+		writer.WriteHeader(200)
+		encoder := json.NewEncoder(writer)
+		encoder.Encode(WebResponse{
+			Data: "Successfuly updated products",
+		})
+	})
+
+	router.DELETE("/products/:id", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		id := params.ByName("id")
+
+		_, err := db.Exec(context.Background(), "DELETE FROM products WHERE id=$1", id)
+		if err != nil {
+			panic(err)
+		}
+
+		writer.Header().Add("Content-Type", "application/json")
+		writer.WriteHeader(200)
+		encoder := json.NewEncoder(writer)
+		encoder.Encode(WebResponse{
+			Data: "Successfuly deleted products",
+		})
+	})
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+
+}
